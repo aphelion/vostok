f1b414051ca8e427b7d386c3dd560dfe8e13eef3
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-07-22 22:10:01 -0700

    Disable www subdomain redirection to the apex

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  constraints(host: /^www\./i) do
-    match '(*any)' => redirect {
-              |params, request| URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '')}.to_s
-          }, via: :all
-  end
+  # constraints(host: /^www\./i) do
+  #   match '(*any)' => redirect {
+  #             |params, request| URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '')}.to_s
+  #         }, via: :all
+  # end
 
   root 'static_pages#home'
 end
